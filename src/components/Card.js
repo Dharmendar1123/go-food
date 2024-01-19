ff8d2c8d53f755724cb18061dfcc3e1bbd68e9e7
Dharmendar Parmar <Dharmendar.Parmar@parkar.local>
2024-01-19 19:09:49 +0530

    create react components

---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <div className="card mt-3" style={{ width: "18rem" }}>
+        <img src="https://images.unsplash.com/photo-1607532941433-304659e8198a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWRzfGVufDB8fDB8fHww" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">This is some important text.</p>
+          <div className="container w-100">
+            <select className="m-2 h-100 bg-success rounded">
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+            <select className="m-2 h-100 bg-success rounded">
+              <option value="half">Half</option>
+              <option value="full">Full</option>
+            </select>
+            <div className="d-inline h-100 fs-5">Total Price</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
